5fca9469bc98adbd230f35e62607ba3c17b978d0
kevin <kev.aubree@gmail.com>
2025-03-20 01:19:43 +0100

    feat: add .gitignore

--- dist/config/rateLimit.js
+++
@@ -1,6 +0,0 @@
-export const rateLimitConfig = {
-    limit: 5, // 5 requests max
-    interval: 10000, // Over a 10-second period
-    windowCleanup: 60000 // Clean entries older than 1 minute
-};
-export default rateLimitConfig;
